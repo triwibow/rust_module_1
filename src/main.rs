fcce36a46f5c2cc31f7bc9f3534e4f973cd46554
Tri Wibowo <triw@oninyon.com>
2023-11-21 15:09:50 +0700

    - mod

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 mod my_io;
 mod my_number;
+
+use my_number::conversion::{string_to_number, is_odd_number};
+use my_number::type_check::type_of;
+
 fn main() {
 	println!("Enter number");
 	let input_number = my_io::read_entry();
 	println!("Your phone number : {}" , input_number);
 
-	let number = my_number::string_to_number(input_number);
-	let is_odd = my_number::is_odd_number(number);
+	let number = string_to_number(input_number);
+	let is_odd = is_odd_number(number);
 
 	println!("is {number} is odd ? {is_odd}");
+
+	println!("tipe of number is {}", type_of(&number));
+	println!("tipe of number is {}", type_of(&is_odd));
+
 
 }
--- src/my_number/mod.rs
+++ src/my_number/mod.rs
@@ -1,7 +1,4 @@
-pub fn string_to_number(text:String) -> i32 {
-  text.parse::<i32>().unwrap()
-}
+pub mod conversion;
 
-pub fn is_odd_number(number:i32) -> bool {
-  number % 2 == 0
-}
+#[path ="check_type.rs"]
+pub mod type_check;
